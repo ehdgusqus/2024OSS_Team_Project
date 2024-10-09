8b9741676a484f4a2c9892276d56a73ab241890c
ehdgusqus <ehdgusqus55@gmail.com>
2024-10-10 01:36:12 +0900

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // React Router import
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MainContent from './components/MainContent';
+import SignupPage from './pages/FristPage/Signup'; 
+import './App.css'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-container">
+        <Header />
+        <div className="content-container">
+          <Routes>
+            <Route path="/" element={<MainContent />} /> {/* 기본 경로로 MainContent 설정 */}
+            <Route path="/signup" element={<SignupPage />} /> {/* 회원가입 페이지 */}
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
