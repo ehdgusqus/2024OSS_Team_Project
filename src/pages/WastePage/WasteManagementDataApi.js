bbc684a2d6cb16e93fe46e84078fc55e27dccec5
Jimin127 <jm22200409@handong.ac.kr>
2024-10-20 16:06:37 +0900

    update Waste

--- src/pages/WastePage/WasteManagementDataApi.js
+++ src/pages/WastePage/WasteManagementDataApi.js
@@ -2,20 +2,31 @@ import React, { useState, useEffect } from 'react';
 import { Bar } from 'react-chartjs-2';
 import 'chart.js/auto';
 
-const WasteManagementData = () => {
+const WasteManagementDataApi = () => {
   const [wasteData, setWasteData] = useState([]);
   const [selectedCity, setSelectedCity] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('/data.json');
-        const data = await response.json();
-        setWasteData(data.data);
+        const response = await fetch(
+          '/sds/JsonApi.do?PID=NTN001&YEAR=2019&USRID=jamie123&KEY=KZMLYLCT2O96ZVGF9UD3F2IC7FY6G8CKVPJXAOJU7RXDO'
+        );
+        const jsonData = await response.json();
+        
+        console.log("API에서 받아온 데이터:", jsonData); 
+        
+        if (jsonData && jsonData.data) {
+          setWasteData(jsonData.data);
+          console.log("추출된 데이터:", jsonData.data);
+        } else {
+          console.error('잘못된 데이터 형식:', jsonData);
+        }
       } catch (error) {
         console.error('데이터를 가져오는 중 오류 발생:', error);
       }
     };
+
     fetchData();
   }, []);
 
@@ -128,4 +139,4 @@ const WasteManagementData = () => {
   );
 };
 
-export default WasteManagementData;
+export default WasteManagementDataApi;
