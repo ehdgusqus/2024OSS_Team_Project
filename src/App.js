bc11bf2f56965da69e5acd7a743ab886d2fc7d35
ehdgusqus <ehdgusqus55@gmail.com>
2024-10-20 18:45:04 +0900

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MainLayout from './components/MainLayout'; 
-import MainPage from './pages/MainPage'; 
+import MainLayout from './components/MainLayout';
+import MainPage from './pages/MainPage';
 import ActivityManagement from './pages/ActivityManagement';
-import SignupPage from './pages/SignupPage'; 
-import UserManagement from './pages/UserManagement'; // UserManagement 컴포넌트 추가
+import SignupPage from './pages/SignupPage';
+import UserManagement from './pages/UserManagement';
+import ShowGoal from './pages/GoalPage/Goal/ShowGoal';
+import CreateGoal from './pages/GoalPage/Goal/CreateGoal';
+import Goal from './pages/GoalPage/Goal/Goal';
+import GoalEdit from './pages/GoalPage/Goal/GoalEdit';
+import ShowCommunity from './pages/CommunityPage/Community/ShowCommunity';
+import CreateCommunity from './pages/CommunityPage/Community/CreateCommunity';
+import CommunityEdit from './pages/CommunityPage/Community/CommunityEdit';
 
 function App() {
-    const [selectedUserId, setSelectedUserId] = useState(null); // 선택된 유저 ID 상태 추가
+    const [selectedUserId, setSelectedUserId] = useState(null);
 
     return (
         <Router>
             <Routes>
                 <Route path="/" element={
                     <MainLayout>
-                        <MainPage selectedUserId={selectedUserId} /> {/* 선택된 유저 ID 전달 */}
+                        <MainPage selectedUserId={selectedUserId} />
                     </MainLayout>
                 } />
                 <Route path="/signup" element={
@@ -29,7 +36,42 @@ function App() {
                 } />
                 <Route path="/user-management" element={
                     <MainLayout>
-                        <UserManagement setSelectedUserId={setSelectedUserId} /> {/* 선택된 유저 ID 설정하는 함수 전달 */}
+                        <UserManagement setSelectedUserId={setSelectedUserId} />
+                    </MainLayout>
+                } />
+                <Route path="/goals" element={
+                    <MainLayout>
+                        <ShowGoal />
+                    </MainLayout>
+                } />
+                <Route path="/create-goal" element={
+                    <MainLayout>
+                        <CreateGoal />
+                    </MainLayout>
+                } />
+                <Route path="/goal/:id" element={
+                    <MainLayout>
+                        <Goal />
+                    </MainLayout>
+                } />
+                <Route path="/edit-goal/:id" element={
+                    <MainLayout>
+                        <GoalEdit /> 
+                    </MainLayout>
+                } />
+                <Route path="/show-community" element={
+                    <MainLayout>
+                        <ShowCommunity />
+                    </MainLayout>
+                } />
+                <Route path="/create-community" element={
+                    <MainLayout>
+                        <CreateCommunity />
+                    </MainLayout>
+                } />
+                <Route path="/edit-community/:id" element={
+                    <MainLayout>
+                        <CommunityEdit /> 
                     </MainLayout>
                 } />
             </Routes>
