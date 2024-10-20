63f3c20f778aeea2f1e880715dc4ec0617906980
ehdgusqus <ehdgusqus55@gmail.com>
2024-10-20 20:11:40 +0900

    update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Link 컴포넌트 추가
-import { Layout, Typography } from 'antd';
+import { Link, useNavigate } from 'react-router-dom'; 
+import { Layout, Typography, Button } from 'antd';
 
 const { Header } = Layout;
 const { Title } = Typography;
 
 const AppHeader = ({ setIsLoggedIn }) => {
+    const navigate = useNavigate(); // 
+
+    const handleWastePageNavigation = () => {
+        navigate('/waste-management'); // wastePage로 이동
+    };
+
     return (
         <Header style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', backgroundColor: '#001529' }}>
             <Link to="/" style={{ color: 'white', textDecoration: 'none' }}> 
                 <Title level={2} style={{ color: 'white', margin: 0 }}>EcoHabit</Title>
             </Link>
+            <Button 
+                type="primary" 
+                style={{ marginLeft: 'auto' }} 
+                onClick={handleWastePageNavigation} 
+            >
+                Waste
+            </Button>
         </Header>
     );
 };
